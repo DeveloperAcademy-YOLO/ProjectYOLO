8dfd6a02c519fb4b7a84c18c4ba4d184cc003bf7
PJunyeong <j_aion@naver.com>
2022-10-04 19:29:54 +0900

    [feat] #8 DeleteUser + UpdateProfile

--- RollingPaper/RollingPaper/Managers/FirebaseAuthManager.swift
+++ RollingPaper/RollingPaper/Managers/FirebaseAuthManager.swift
@@ -12,10 +12,17 @@ import Combine
 protocol AuthManager {
     func signIn(email: String, password: String) -> AnyPublisher<Bool, Error>
     func signUp(email: String, password: String, name: String) -> AnyPublisher<Bool, Error>
+    func signOut() -> AnyPublisher<Bool, Error>
+    func deleteUser() -> AnyPublisher<Bool, Error>
+    func updateUserProfile(name: String?, photoURLString: String?) -> AnyPublisher<Bool, Error>
 }
 
 final class FirebaseAuthManager: AuthManager {
     private let auth = FirebaseAuth.Auth.auth()
+    
+    enum AuthError: LocalizedError {
+        case userNotExists
+    }
     
     func signUp(email: String, password: String, name: String) -> AnyPublisher<Bool, Error> {
         return Future<Bool, Error> { [weak self] promise in
@@ -38,6 +45,61 @@ final class FirebaseAuthManager: AuthManager {
                 } else {
                     promise(.success(true))
                 }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func signOut() -> AnyPublisher<Bool, Error> {
+        return Future<Bool, Error> { [weak self] promise in
+            do {
+                try self?.auth.signOut()
+                promise(.success(true))
+            } catch {
+                promise(.failure(error))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func deleteUser() -> AnyPublisher<Bool, Error> {
+        return Future<Bool, Error> { [weak self] promise in
+            if let user = self?.auth.currentUser {
+                user.delete { error in
+                    if let error = error {
+                        promise(.failure(error))
+                    } else {
+                        promise(.success(true))
+                    }
+                }
+            } else {
+                promise(.failure(AuthError.userNotExists))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func updateUserProfile(name: String? = nil, photoURLString: String? = nil) -> AnyPublisher<Bool, Error> {
+        return Future<Bool, Error> { [weak self] promise in
+            if let user = self?.auth.currentUser {
+                let changeRequest = user.createProfileChangeRequest()
+                if let name = name {
+                    changeRequest.displayName = name
+                }
+                if
+                    let photoURLString = photoURLString,
+                    let photoURL = URL(string: photoURLString) {
+                    changeRequest.photoURL = photoURL
+                }
+                changeRequest.commitChanges { error in
+                    if let error = error {
+                        promise(.failure(error))
+                    } else {
+                        promise(.success(true))
+                    }
+                }
+            } else {
+                promise(.failure(AuthError.userNotExists))
             }
         }
         .eraseToAnyPublisher()
