594d28e904a66c73848e647035bc84ba6ea79268
PJunyeong <j_aion@naver.com>
2022-10-04 13:58:37 +0900

    [feat] #6 lint removed

--- RollingPaper/RollingPaper/Controllers/MainViewController.swift
+++ RollingPaper/RollingPaper/Controllers/MainViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
--- RollingPaper/RollingPaper/Resources/SceneDelegate.swift
+++ RollingPaper/RollingPaper/Resources/SceneDelegate.swift
@@ -11,7 +11,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
@@ -48,7 +47,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
 
