19b3a260540d8d4c82d9b62e2f43f0e20277955f
PJunyeong <j_aion@naver.com>
2022-10-05 15:34:25 +0900

    [feat] #22 PaperModelFile Directory

--- RollingPaper/RollingPaper/Managers/FirestoreManager.swift
+++ RollingPaper/RollingPaper/Managers/FirestoreManager.swift
@@ -8,7 +8,6 @@
 import Foundation
 import Combine
 
-
 final class FirestoreManager {
     
 }
---
+++ RollingPaper/RollingPaper/Managers/LocalDatabaseManager.swift
@@ -0,0 +1,13 @@
+//
+//  LocalDatabaseManager.swift
+//  RollingPaper
+//
+//  Created by Junyeong Park on 2022/10/05.
+//
+
+import Foundation
+import Combine
+
+protocol LocalDatabaseManager {
+    var papersSubject: PassthroughSubject<[PaperModel], Error> {get set}
+}
