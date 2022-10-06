7138dc75f77cd9a3d751634eb4422470f201316a
PJunyeong <j_aion@naver.com>
2022-10-06 19:43:57 +0900

    [feat] #17 default layout

--- RollingPaper/RollingPaper/Controllers/SignInViewController.swift
+++ RollingPaper/RollingPaper/Controllers/SignInViewController.swift
@@ -13,7 +13,8 @@ import AuthenticationServices
 class SignInViewController: UIViewController {
     private let emailTextField: UITextField = {
         let textField = UITextField()
-        textField.borderStyle = .roundedRect
+        textField.layer.cornerRadius = 12
+        textField.borderRect(forBounds: <#T##CGRect#>)
         return textField
     }()
     private let passwordTextField: UITextField = {
@@ -39,6 +40,16 @@ class SignInViewController: UIViewController {
         let button = ASAuthorizationAppleIDButton(type: .signIn, style: .black)
         return button
     }()
+    private let waringImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .center
+        return imageView
+    }()
+    private let waringLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .systemGray
+        return label
+    }()
     
     private let viewModel = SignInViewModel()
     private var cancellables = Set<AnyCancellable>()
@@ -52,14 +63,38 @@ class SignInViewController: UIViewController {
     
     private func setSignInViewUI() {
         view.backgroundColor = .systemBackground
-        view.addSubview(<#T##view: UIView##UIView#>)
+        view.addSubviews([emailTextField, passwordTextField, signInButton, appleSignInButton])
+        emailTextField.snp.makeConstraints({ make in
+            make.top.equalToSuperview().offset((view.frame.height - 246) / 2)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(38)
+            make.width.equalTo(380)
+        })
+        passwordTextField.snp.makeConstraints({ make in
+            make.top.equalTo(emailTextField.snp.bottom).offset(28)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(38)
+            make.width.equalTo(380)
+        })
+        signInButton.snp.makeConstraints({ make in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(28)
+            make.width.equalTo(380)
+            make.height.equalTo(48)
+            make.centerX.equalToSuperview()
+        })
+        appleSignInButton.snp.makeConstraints({ make in
+            make.top.equalTo(signInButton.snp.bottom).offset(28)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(48)
+            make.width.equalTo(380)
+        })
     }
     
     private func bind() {
         let output = viewModel.transform(input: input.eraseToAnyPublisher())
         output
             .receive(on: DispatchQueue.main)
-            .sink { [weak self] receivedValue in
+            .sink(receiveValue: { [weak self] receivedValue in
                 guard self != nil else { return }
                 switch receivedValue {
                 case .signInDidFail(error: let error):
@@ -73,37 +108,37 @@ class SignInViewController: UIViewController {
                     print("Successfully Signed In")
                 // Success -> Into current view flows...
                 }
-            }
+            })
             .store(in: &cancellables)
         signInButton
             .tapPublisher
-            .sink { [weak self] _ in
+            .sink(receiveValue: { [weak self] _ in
                 guard let self = self else { return }
                 self.input.send(.signInButtonTap)
-            }
+            })
             .store(in: &cancellables)
         appleSignInButton
             .controlPublisher(for: .touchUpInside)        
-            .sink { [weak self] _ in
+            .sink(receiveValue: { [weak self] _ in
                 guard let self = self else { return }
                 self.input.send(.appleSignInButtonTap)
-            }
+            })
             .store(in: &cancellables)
         emailTextField
             .textPublisher
             .compactMap({ $0 })
-            .sink { [weak self] email in
+            .sink(receiveValue: { [weak self] email in
                 guard let self = self else { return }
                 self.viewModel.email.send(email)
-            }
+            })
             .store(in: &cancellables)
         passwordTextField
             .textPublisher
             .compactMap({ $0 })
-            .sink { [weak self] password in
+            .sink(receiveValue: { [weak self] password in
                 guard let self = self else { return }
                 self.viewModel.password.send(password)
-            }
+            })
             .store(in: &cancellables)
     }
 }
--- RollingPaper/RollingPaper/Resources/UIView+Extensions.swift
+++ RollingPaper/RollingPaper/Resources/UIView+Extensions.swift
@@ -9,7 +9,6 @@ import Foundation
 import UIKit
 
 extension UIView {
- 
     func addSubviews(_ views: [UIView]) {
         for view in views {
             addSubview(view)
