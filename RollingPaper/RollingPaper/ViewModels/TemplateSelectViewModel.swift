f597aecb654d3fc2f91256d019b53fbe8ab0c364
ddophi98 <lightcloud98@gmail.com>
2022-10-06 10:43:02 +0900

    [feat] #19 make view model

---
+++ RollingPaper/RollingPaper/ViewModels/TemplateSelectViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  TemplateSelectViewModel.swift
+//  RollingPaper
+//
+//  Created by 김동락 on 2022/10/05.
+//
+
+import UIKit
+
+final class TemplateSelectViewModel {
+    
+    func getRecentTemplate() -> TemplateEnum? {
+        // TODO: 최근 템플릿 받아오기
+        let recentTemplate: TemplateEnum = .grid
+        return recentTemplate
+    }
+    
+    func getTemplates() -> [TemplateEnum] {
+        // TODO: 현재 존재하는 템플릿 받아오기
+        let templates: [TemplateEnum] = [.halloween, .grid, .sunrise]
+        return templates
+    }
+}
