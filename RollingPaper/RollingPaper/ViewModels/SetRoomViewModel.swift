42218026b21cbd021e641b2294886fc0ca478986
ddophi98 <lightcloud98@gmail.com>
2022-10-07 15:09:12 +0900

    [feat] #33 make simple viewmodel

---
+++ RollingPaper/RollingPaper/ViewModels/SetRoomViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SetRoomViewModel.swift
+//  RollingPaper
+//
+//  Created by 김동락 on 2022/10/07.
+//
+
+import UIKit
+
+class SetRoomViewModel {
+    var paperModel: PaperModel?
+    
+    
+}
