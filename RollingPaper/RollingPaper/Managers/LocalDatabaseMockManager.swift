d29bddb3dac2e287b7f3c91c888c9d8d6969dd7b
PJunyeong <j_aion@naver.com>
2022-10-08 16:20:50 +0900

    [feat] #40 loadPaper as private func

--- RollingPaper/RollingPaper/Managers/LocalDatabaseMockManager.swift
+++ RollingPaper/RollingPaper/Managers/LocalDatabaseMockManager.swift
@@ -7,11 +7,36 @@
 
 import Foundation
 import Combine
+import UIKit
 
 final class LocalDatabaseMockManager: LocalDatabaseManager {
-    static var shared: LocalDatabaseManager
+    static let shared: LocalDatabaseManager = LocalDatabaseMockManager()
     
-    var papersSubject: CurrentValueSubject<[PaperModel], Never>
+    var papersSubject: CurrentValueSubject<[PaperModel], Never> = .init([])
+    
+    private init() {
+        // Set originally set data as default paper data
+        loadPapers()
+    }
+    
+    private func loadPapers() {
+        var papers = [PaperModel]()
+        let today = Date()
+        let tomorrow = Calendar.current.date(byAdding: .day, value: 1, to: Date())!
+        var paper1 = PaperModel(cards: [], date: Date(), endTime: tomorrow, title: "MockPaper1", templateString: TemplateEnum.halloween.rawValue)
+        var paper2 = PaperModel(cards: [], date: Date(), endTime: tomorrow, title: "MockPaper2", templateString: TemplateEnum.grid.rawValue)
+        if
+            let mockImage = UIImage(systemName: "person"),
+            let mockData = mockImage.jpegData(compressionQuality: 0.8) {
+            let card1 = CardModel(date: Date(), content: mockData)
+            let card2 = CardModel(date: Date(), content: mockData)
+            let card3 = CardModel(date: Date(), content: mockData)
+            let card4 = CardModel(date: Date(), content: mockData)
+            paper1.cards.append(contentsOf: [card1, card2])
+            paper2.cards.append(contentsOf: [card3, card4])
+            papers.append(contentsOf: [paper1, paper2])
+        }
+    }
     
     func addPaper(paper: PaperModel) {
     }
