f0a6e85a5bb84d89b04489e1606084a3c435ae86
PJunyeong <j_aion@naver.com>
2022-10-08 18:10:51 +0900

    [feat] #40 Custom TextField

--- RollingPaper/RollingPaper/Views/SignInTextField.swift
+++ RollingPaper/RollingPaper/Views/SignInTextField.swift
@@ -6,7 +6,95 @@
 //
 
 import UIKit
+import SnapKit
 
-class SignInTextField: UITextField {
-
+class SignInTextField: UIView {
+    enum SignInTextFieldEnum {
+        case email
+        case password
+        case name
+    }
+    
+    enum SignInTextFieldState {
+        case normal
+        case focused
+        case waring
+        case passed
+    }
+    
+    private var textFieldEnum: SignInTextFieldEnum?
+    
+    private let checkImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .center
+        imageView.image = UIImage(systemName: "checkmark.circle")?.withTintColor(.systemGreen, renderingMode: .alwaysOriginal)
+        return imageView
+    }()
+    
+    private let nameCountView: UILabel = {
+        let label = UILabel()
+        label.text = "0/8"
+        label.textColor = .white
+        label.backgroundColor = .systemGray
+        label.layer.cornerRadius = 9
+        label.layer.masksToBounds = true
+        return label
+    }()
+    
+    private let textField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.layer.masksToBounds = true
+        textField.layer.cornerRadius = 12
+        textField.layer.borderColor = UIColor.systemGray.cgColor
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 17, height: textField.frame.height))
+        textField.leftView = paddingView
+        textField.leftViewMode = .always
+        return textField
+    }()
+        
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setTextfieldUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setTextFieldUI() {
+        addSubview(textField)
+        addSubview(checkImageView)
+        addSubview(nameCountView)
+        textField.frame = self.bounds
+    }
+    
+    func setTextFieldType(type: SignInTextFieldEnum) {
+        self.textFieldEnum = type
+        switch type {
+        case .email: break
+        case .password: break
+        case .name: break
+        }
+    }
+    
+    func setTextFieldState(state: SignInTextFieldState) {
+        guard let currentTextField = textFieldEnum else { return }
+        switch currentTextField {
+        case .email, .password:
+            switch state {
+            case .normal: break
+            case .focused: break
+            case .waring: break
+            case .passed: break
+            }
+        case .name:
+            switch state {
+            case .normal: break
+            case .focused: break
+            case .waring: break
+            case .passed: break
+            }
+        }
+    }
 }
