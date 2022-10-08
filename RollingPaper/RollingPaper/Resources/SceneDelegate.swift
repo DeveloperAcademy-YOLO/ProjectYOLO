d16ff44ff04caddd1ed798c36b46ae8e1f7f589d
PJunyeong <j_aion@naver.com>
2022-10-08 16:39:40 +0900

    [refactoring] #40 past component cleaning

--- RollingPaper/RollingPaper/Resources/SceneDelegate.swift
+++ RollingPaper/RollingPaper/Resources/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = SignInViewController()
+        window.rootViewController = SignUpViewController()
         window.makeKeyAndVisible()
         self.window = window
     }
