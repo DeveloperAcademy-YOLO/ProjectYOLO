dec42837db2e634ce87160f3c6e8b51a07cdce1d
PJunyeong <j_aion@naver.com>
2022-10-08 16:28:15 +0900

    [feat] #40 paper and card CRUD

--- RollingPaper/RollingPaper/Managers/LocalDatabaseMockManager.swift
+++ RollingPaper/RollingPaper/Managers/LocalDatabaseMockManager.swift
@@ -36,25 +36,60 @@ final class LocalDatabaseMockManager: LocalDatabaseManager {
             paper2.cards.append(contentsOf: [card3, card4])
             papers.append(contentsOf: [paper1, paper2])
         }
+        papersSubject.send(papers)
     }
     
     func addPaper(paper: PaperModel) {
+        papersSubject.send(papersSubject.value + [paper])
     }
     
     func addCard(paperId: String, card: CardModel) {
+        var currentPapers = papersSubject.value
+        if let index = currentPapers.firstIndex(where: { $0.paperId == paperId }) {
+            var paper = currentPapers[index]
+            paper.cards.append(card)
+            currentPapers[index] = paper
+            papersSubject.send(currentPapers)
+        }
     }
     
     func removePaper(paper: PaperModel) {
+        var currentPapers = papersSubject.value
+        if let index = currentPapers.firstIndex(where: { $0.paperId == paper.paperId }) {
+            currentPapers.remove(at: index)
+            papersSubject.send(currentPapers)
+        }
     }
     
     func removeCard(paperId: String, card: CardModel) {
+        var currentPapers = papersSubject.value
+        if let index = currentPapers.firstIndex(where: { $0.paperId == paperId }) {
+            var paper = currentPapers[index]
+            if let cardIndex = paper.cards.firstIndex(where: { $0.cardId == card.cardId }) {
+                paper.cards.remove(at: cardIndex)
+                currentPapers[index] = paper
+                papersSubject.send(currentPapers)
+            }
+        }
     }
     
     func updatePaper(paper: PaperModel) {
+        var currentPapers = papersSubject.value
+        if let index = currentPapers.firstIndex(where: { $0.paperId == paper.paperId }) {
+            currentPapers[index] = paper
+            papersSubject.send(currentPapers)
+        }
     }
     
     func updateCard(paperId: String, card: CardModel) {
+        var currentPapers = papersSubject.value
+        if let index = currentPapers.firstIndex(where: { $0.paperId == paperId }) {
+            var paper = currentPapers[index]
+            if let cardIndex = paper.cards.firstIndex(where: { $0.cardId == card.cardId }) {
+                paper.cards[cardIndex] = card
+                currentPapers[index] = paper
+                papersSubject.send(currentPapers)
+            }
+        }
     }
 }
-
-
