7092ec8d5ca2fef186a0504eac321ea814c9015a
PJunyeong <j_aion@naver.com>
2022-10-11 19:44:09 +0900

    [feat] #40 keyboard height dynamic

--- RollingPaper/RollingPaper/Controllers/SignUpViewController.swift
+++ RollingPaper/RollingPaper/Controllers/SignUpViewController.swift
@@ -39,6 +39,7 @@ class SignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setSignUpViewUI()
+        setKeyboardObserver()
     }
     
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
@@ -47,6 +48,25 @@ class SignUpViewController: UIViewController {
             make.top.equalToSuperview().offset(topOffset)
         })
         view.layoutIfNeeded()
+    }
+    
+    private func setKeyboardObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
     }
     
     private func setSignUpViewUI() {
