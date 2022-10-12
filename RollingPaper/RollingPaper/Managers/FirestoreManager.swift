c3b05f0a047593562fe39ef2ae697ce63b1ceda7
PJunyeong <j_aion@naver.com>
2022-10-12 15:17:01 +0900

    [feat] #59 FireStore Manager loadPapers

--- RollingPaper/RollingPaper/Managers/FirestoreManager.swift
+++ RollingPaper/RollingPaper/Managers/FirestoreManager.swift
@@ -8,18 +8,56 @@
 import Foundation
 import FirebaseFirestore
 import Combine
+import CombineCocoa
 
 final class FirestoreManager: DatabaseManager {
+    enum Constants: String {
+        case usersCollectionPath = "rollingpaper_users"
+        case userPaperPath = "user_papers"
+        case PapersPath = "rollingpaper_papers"
+    }
+    
     static let shared: DatabaseManager = FirestoreManager()
     var cardsSubject: CurrentValueSubject<[CardModel], Never> = .init([])
     var papersSubject: CurrentValueSubject<[PaperModel], Never> = .init([])
     private let database = Firestore.firestore()
+    private let authManager: AuthManager
+    private var cancellables = Set<AnyCancellable>()
+    private var currentUserEmail: String?
     
-    private init() {
+    private init(authManager: AuthManager = FirebaseAuthManager.shared) {
+        self.authManager = authManager
+        bind()
         loadPapers()
     }
     
     private func loadPapers() {
+        guard let currentUserEmail = currentUserEmail else { return }
+        database
+            .collection(Constants.usersCollectionPath.rawValue)
+            .document(currentUserEmail)
+            .collection(Constants.userPaperPath.rawValue)
+            .getDocuments(completion: { [weak self] querySnapshot, error in
+                guard let self = self else { return }
+                guard
+                    let documents = querySnapshot?.documents,
+                    error == nil else { return }
+                let papers = documents
+                    .map({ $0.data() })
+                    .compactMap({ try? JSONSerialization.data(withJSONObject: $0, options: [.fragmentsAllowed])})
+                    .compactMap({ try? JSONDecoder().decode(PaperModel.self, from: $0)})
+                self.papersSubject.send(papers)
+            })
+    }
+    
+    private func bind() {
+        authManager
+            .userProfileSubject
+            .compactMap({ $0 })
+            .sink(receiveValue: { [weak self] userProfile in
+                self?.currentUserEmail = userProfile.email
+            })
+            .store(in: &cancellables)
     }
     
     func addPaper(paper: PaperModel) {
