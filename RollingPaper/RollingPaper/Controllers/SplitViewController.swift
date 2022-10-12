ba1b5d85a94fbdff11dd9e9917fc45293dce39ac
Jin-won Choi(Kelly Chui) <optioneldar@gmail.com>
2022-10-13 01:31:14 +0900

    [fix] #54 Remove NavigationController Associations Between Categories

--- RollingPaper/RollingPaper/Controllers/SplitViewController.swift
+++ RollingPaper/RollingPaper/Controllers/SplitViewController.swift
@@ -10,27 +10,30 @@ import UIKit
 class SplitViewController: UISplitViewController, UISplitViewControllerDelegate, SidebarViewControllerDelegate {
     
     func didSelectCategory(_ category: CategoryModel) {
+        let sidebar = UINavigationController(rootViewController: self.sidebarViewController)
+        let templateViewController = UINavigationController(rootViewController: self.templateViewController)
+        let mainViewController = UINavigationController(rootViewController: self.mainViewController)
+        
         switch category.name {
         case "페이퍼 템플릿":
-            secondaryViewController?.navigationController?.popToRootViewController(false, completion: {self.secondaryViewController.navigationController?.pushViewController(TemplateSelectViewController(), animated: false)})
+            self.viewControllers[1] = templateViewController
         case "페이퍼 보관함":
-            secondaryViewController?.navigationController?.popToRootViewController(false, completion: {self.secondaryViewController.navigationController?.pushViewController(TemplateSelectViewController(), animated: false)})
+            self.viewControllers[1] = mainViewController
         case "설정":
-            secondaryViewController?.navigationController?.popToRootViewController(false, completion: {self.secondaryViewController.navigationController?.pushViewController(TemplateSelectViewController(), animated: false)})
+            self.viewControllers[1] = mainViewController
         default:
-            break
+            self.viewControllers[1] = mainViewController
         }
     }
-    
     
     var sideBarCategories: [CategoryModel] = [
         CategoryModel(name: "페이퍼 템플릿", icon: "doc.on.doc"),
         CategoryModel(name: "페이퍼 보관함", icon: "folder"),
         CategoryModel(name: "설정", icon: "gearshape")
     ]
-    
     private var sidebarViewController: SidebarViewController!
-    private var secondaryViewController: UIViewController!
+    private var templateViewController: TemplateSelectViewController!
+    private var mainViewController: MainViewController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,8 +57,11 @@ class SplitViewController: UISplitViewController, UISplitViewControllerDelegate,
     
     private func loadViewControllers() {
         self.sidebarViewController = SidebarViewController()
+        self.templateViewController = TemplateSelectViewController()
+        self.mainViewController = MainViewController()
         self.sidebarViewController.delegate = self
-        self.secondaryViewController = TemplateSelectViewController()
-        self.viewControllers = [sidebarViewController, secondaryViewController]
+        let sidebar = UINavigationController(rootViewController: self.sidebarViewController)
+        let templateViewController = UINavigationController(rootViewController: self.templateViewController)
+        self.viewControllers = [sidebar, templateViewController]
     }
 }
