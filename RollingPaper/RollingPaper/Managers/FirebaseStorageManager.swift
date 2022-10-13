43b8088f83e2beaf7bd92ffc6948fdb64dd8482f
PJunyeong <j_aion@naver.com>
2022-10-13 14:36:10 +0900

    [feat] #66 Storage Manager get/download funcs

--- RollingPaper/RollingPaper/Managers/FirebaseStorageManager.swift
+++ RollingPaper/RollingPaper/Managers/FirebaseStorageManager.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import FirebaseStorage
+import Combine
 
 final class FirebaseStorageManager {
     enum CardContentType: String {
@@ -37,6 +38,31 @@ final class FirebaseStorageManager {
         })
     }
     
+    static func uploadData(dataId: String, data: Data, contentType: CardContentType, pathRoot: String) -> AnyPublisher<URL?, Error> {
+        let metadata = StorageMetadata()
+        metadata.contentType = contentType.rawValue
+        let reference = Storage.storage().reference().child("\(dataId)")
+        return Future({ promise in
+            reference.putData(data, metadata: metadata, completion: { _, error in
+                if let error = error {
+                    print(error.localizedDescription)
+                    promise(.failure(error))
+                } else {
+                    print("Storage data put Succeed")
+                    reference.downloadURL(completion: { url, error in
+                        if let error = error {
+                            print(error.localizedDescription)
+                            promise(.failure(error))
+                        } else {
+                            promise(.success(url))
+                        }
+                    })
+                }
+            })
+        })
+        .eraseToAnyPublisher()
+    }
+    
     static func downloadData(urlString: String, maxSize: Int64 = Int64(1 * 1024 * 1024), completion: @escaping (Result<Data?, Error>) -> Void) {
         let reference = Storage.storage().reference(forURL: urlString)
         reference.getData(maxSize: maxSize, completion: { data, error in
@@ -46,5 +72,21 @@ final class FirebaseStorageManager {
                 completion(.success(data))
             }
         })
+    }
+    
+    static func downloadData(urlString: String, maxSize: Int64 = Int64(1 * 1024 * 1024)) -> AnyPublisher<Data?, Error> {
+        let reference = Storage.storage().reference(forURL: urlString)
+        return Future({ promise in
+            reference.getData(maxSize: maxSize, completion: { data, error in
+                if let error = error {
+                    print(error.localizedDescription)
+                    promise(.failure(error))
+                } else {
+                    print("Data Downloaded Successfully")
+                    promise(.success(data))
+                }
+            })
+        })
+        .eraseToAnyPublisher()
     }
 }
