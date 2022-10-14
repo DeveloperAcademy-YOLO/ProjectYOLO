d14e93706da96674135f9c4756a625c4fb971a6e
Yosep <dlstj1212@naver.com>
2022-10-14 14:21:25 +0900

    [feat] #69 Add UIimageView in CardResultView

--- RollingPaper/RollingPaper/Controllers/Card/CardResultViewController.swift
+++ RollingPaper/RollingPaper/Controllers/Card/CardResultViewController.swift
@@ -11,9 +11,35 @@ import Combine
 
 final class CardResultViewController: UIViewController {
  
+    var backgroundImg = UIImage(named: "Rectangle")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .blue
+        
+        view.addSubview(someImageView)
+        someImageView.backgroundColor = .white
+        someImageView.layer.masksToBounds = true
+        someImageView.layer.cornerRadius = 50
+        someImageView.contentMode = .scaleAspectFill
+        someImageView.image = backgroundImg
+        someImageViewConstraints()
     }
     
+    lazy var someImageView: UIImageView = {
+        let theImageView = UIImageView()
+        theImageView.backgroundColor = .white
+        theImageView.translatesAutoresizingMaskIntoConstraints = false
+        theImageView.isUserInteractionEnabled = true
+        return theImageView
+    }()
+    
+    func someImageViewConstraints() {
+        someImageView.snp.makeConstraints({ make in
+            make.width.equalTo(813)
+            make.height.equalTo(515)
+            make.centerX.equalTo(self.view)
+            make.centerY.equalTo(self.view)
+        })
+    }
 }
