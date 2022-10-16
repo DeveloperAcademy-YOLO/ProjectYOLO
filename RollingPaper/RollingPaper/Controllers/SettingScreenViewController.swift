2917e0c415a336e978b6ec450216bc3f5de428ca
Grizzly <dydrhks02@naver.com>
2022-10-16 17:22:12 +0900

    [feat] #56 button and blur image

--- RollingPaper/RollingPaper/Controllers/SettingScreenViewController.swift
+++ RollingPaper/RollingPaper/Controllers/SettingScreenViewController.swift
@@ -16,10 +16,34 @@ class SettingScreenViewController: UIViewController {
         setupLayout()
     }
     
+    private let editButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor.clear
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = true
+        button.setTitle("편집", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        return button
+    }()
+    
     private let profileImage: UIImageView = {
        let profileImage = UIImageView()
         profileImage.contentMode = UIView.ContentMode.scaleAspectFill
         profileImage.image = UIImage(named: "Halloween_Pumpkin")
+        
+        let blurEffect = UIBlurEffect(style: .regular)
+        let visualEffectView = UIVisualEffectView(effect: blurEffect)
+        visualEffectView.frame = profileImage.bounds
+        visualEffectView.alpha = 0.5
+        profileImage.addSubview(visualEffectView)
+        
+        
+        return profileImage
+    }()
+    
+    private let blurprofileImage: UIImageView = {
+        let profileImage = UIImageView()
+        profileImage.image = UIImage(systemName: "photo.on.rectangle.angled")
         return profileImage
     }()
     
@@ -48,16 +72,6 @@ class SettingScreenViewController: UIViewController {
         view.layer.borderColor = UIColor.systemPink.cgColor
         return view
     }()
-
-    private let editButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = UIColor.blue
-        button.layer.cornerRadius = 12
-        button.layer.masksToBounds = true
-        button.setTitle("편집", for: .normal)
-        button.setTitleColor(.systemPink, for: .normal)
-        return button
-    }()
     
     private let logoutButton: UIButton = {
         let button = UIButton()
@@ -80,21 +94,42 @@ class SettingScreenViewController: UIViewController {
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
         return button
     }()
+        
+    private func createBlur(_ profile: UIImageView) {
+        let blurEffect = UIBlurEffect(style: .regular)
+        let visualEffectView = UIVisualEffectView(effect: blurEffect)
+        visualEffectView.frame = profile.bounds
+        visualEffectView.alpha = 0.5
+        profile.addSubview(visualEffectView)
+//        view.addSubview(visualEffectView)
+    }
     
     private func setupLayout() {
         
         view.addSubview(profileImage)
+
+        let blurEffect = UIBlurEffect(style: .regular)
+        let visualEffectView = UIVisualEffectView(effect: blurEffect)
+        visualEffectView.frame.self = CGRect(origin: .zero, size: CGSize(width: 200, height: 200))
+//        visualEffectView.frame = profileImage.bounds
+        visualEffectView.alpha = 0.5
+        profileImage.addSubview(visualEffectView)
+        profileImage.addSubview(blurprofileImage)
+        
         view.addSubview(profileText)
         view.addSubview(divideView)
         view.addSubview(logoutButton)
         view.addSubview(resignButton)
         view.addSubview(editButton)
-
         view.backgroundColor = .white
         
         editButton.snp.makeConstraints { make in
+
             make.top.equalTo(37)
-            make.trailing.equalTo(view.snp.trailing).offset(38)
+            make.trailing.equalTo(-38)
+            
+            make.height.equalTo(24)
+            make.width.equalTo(37)
         }
         
         profileImage.snp.makeConstraints ({ make in
@@ -103,6 +138,11 @@ class SettingScreenViewController: UIViewController {
             make.height.equalTo(180)
             make.width.equalTo(180)
         })
+        
+        blurprofileImage.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
         
         profileText.snp.makeConstraints { make in
             make.top.equalTo(profileImage.snp.bottom).offset(47)
@@ -126,6 +166,4 @@ class SettingScreenViewController: UIViewController {
             make.centerX.equalTo(view)
         })
     }
-
 }
-
