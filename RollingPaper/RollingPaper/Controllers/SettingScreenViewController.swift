226a9c133eca44baf92ec668a349b0979a1c6a0a
Grizzly <dydrhks02@naver.com>
2022-10-17 10:26:10 +0900

    [feat] #56 make circle image

--- RollingPaper/RollingPaper/Controllers/SettingScreenViewController.swift
+++ RollingPaper/RollingPaper/Controllers/SettingScreenViewController.swift
@@ -27,17 +27,14 @@ class SettingScreenViewController: UIViewController {
     }()
     
     private let profileImage: UIImageView = {
-       let profileImage = UIImageView()
-        profileImage.contentMode = UIView.ContentMode.scaleAspectFill
+       let profileImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 180, height: 180))
+        profileImage.layer.cornerRadius = profileImage.frame.size.width * 0.5
         profileImage.image = UIImage(named: "Halloween_Pumpkin")
-        
-        let blurEffect = UIBlurEffect(style: .regular)
-        let visualEffectView = UIVisualEffectView(effect: blurEffect)
-        visualEffectView.frame = profileImage.bounds
-        visualEffectView.alpha = 0.5
-        profileImage.addSubview(visualEffectView)
-        
-        
+        profileImage.contentMode = UIView.ContentMode.scaleAspectFit
+        profileImage.layer.borderColor = UIColor.red.cgColor
+        profileImage.layer.borderWidth = 3.0
+//        profileImage.clipsToBounds = true
+//        profileImage.layer.masksToBounds = true
         return profileImage
     }()
     
@@ -46,7 +43,7 @@ class SettingScreenViewController: UIViewController {
         profileImage.image = UIImage(systemName: "photo.on.rectangle.angled")
         return profileImage
     }()
-    
+        
     private let profileText: UITextField = {
         let textField = UITextField()
         textField.layer.masksToBounds = false
@@ -84,6 +81,19 @@ class SettingScreenViewController: UIViewController {
         return button
     }()
     
+    lazy var pencilToggleButton: UIButton = {
+        let button = UIButton()
+        button.setUIImage(systemName: "pencil.and.outline")
+        button.tintColor = .lightGray
+        button.addTarget(self, action: #selector(toggleToolKit(_:)), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func toggleToolKit(_ gesture: UITapGestureRecognizer) {
+        
+    }
+    
+    
     private let resignButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = UIColor.white
@@ -101,17 +111,17 @@ class SettingScreenViewController: UIViewController {
         visualEffectView.frame = profile.bounds
         visualEffectView.alpha = 0.5
         profile.addSubview(visualEffectView)
-//        view.addSubview(visualEffectView)
     }
     
     private func setupLayout() {
         
         view.addSubview(profileImage)
 
-        let blurEffect = UIBlurEffect(style: .regular)
+        let blurEffect = UIBlurEffect(style: .dark)
         let visualEffectView = UIVisualEffectView(effect: blurEffect)
-        visualEffectView.frame.self = CGRect(origin: .zero, size: CGSize(width: 200, height: 200))
-//        visualEffectView.frame = profileImage.bounds
+        visualEffectView.frame.self = CGRect(origin: .zero, size: CGSize(width: 180, height: 180))
+        visualEffectView.layer.cornerRadius = visualEffectView.frame.size.width / 2
+        visualEffectView.clipsToBounds = true
         visualEffectView.alpha = 0.5
         profileImage.addSubview(visualEffectView)
         profileImage.addSubview(blurprofileImage)
@@ -123,14 +133,11 @@ class SettingScreenViewController: UIViewController {
         view.addSubview(editButton)
         view.backgroundColor = .white
         
-        editButton.snp.makeConstraints { make in
-
+        editButton.snp.makeConstraints{make in
             make.top.equalTo(37)
             make.trailing.equalTo(-38)
-            
             make.height.equalTo(24)
-            make.width.equalTo(37)
-        }
+            make.width.equalTo(37) }
         
         profileImage.snp.makeConstraints ({ make in
             make.top.equalTo(200)
@@ -142,6 +149,8 @@ class SettingScreenViewController: UIViewController {
         blurprofileImage.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview()
+            make.height.equalTo(39)
+            make.width.equalTo(48)
         }
         
         profileText.snp.makeConstraints { make in
