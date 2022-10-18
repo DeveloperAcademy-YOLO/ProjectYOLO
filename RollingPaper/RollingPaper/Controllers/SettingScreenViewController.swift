abed9e2d43f0cc741539abced6e9f0c55816a2d6
Grizzly <dydrhks02@naver.com>
2022-10-18 13:43:16 +0900

    [feat] #56  fix settingScreenViewController

--- RollingPaper/RollingPaper/Controllers/SettingScreenViewController.swift
+++ RollingPaper/RollingPaper/Controllers/SettingScreenViewController.swift
@@ -93,7 +93,6 @@ class SettingScreenViewController: UIViewController {
         
     }
     
-    
     private let resignButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = UIColor.white
