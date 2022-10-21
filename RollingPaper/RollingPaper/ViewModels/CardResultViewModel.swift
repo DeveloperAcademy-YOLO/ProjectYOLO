ba30ee11ca43924c99ec526cf40ed8675e96e92f
Yosep <dlstj1212@naver.com>
2022-10-22 01:28:35 +0900

    [feat] #101 Add CardResultViewModel

---
+++ RollingPaper/RollingPaper/ViewModels/CardResultViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  CardResultViewModel.swift
+//  RollingPaper
+//
+//  Created by Yosep on 2022/10/22.
+//
+import UIKit
+import Combine
+
+class CardResultViewModel {
+    private var setCardResult: UIImage = UIImage(named: "Rectangle") ?? UIImage(systemName: "heart.fill")!
+    private let databaseManager: DatabaseManager
+    
+    init(databaseManager: DatabaseManager = LocalDatabaseFileManager.shared) {
+        self.databaseManager = databaseManager
+    }
+    enum Input {
+        case setCardResultImg(result: UIImage)
+        case resultSend
+    }
+    private var cancellables = Set<AnyCancellable>()
+    
+    
+    func transform(input: AnyPublisher<Input, Never>) {
+        input
+            .receive(on: DispatchQueue.global(qos: .background))
+            .sink(receiveValue: { [weak self] event in
+                guard let self = self else {return}
+                switch event {
+                case .setCardResultImg(let result):
+                    self.setCardResult(result: result)
+                case .resultSend:
+                 print("ee")
+                }
+            })
+            .store(in: &cancellables)
+    }
+    private func setCardResult(result: UIImage) {
+        self.setCardResult = result
+    }
+}
+    
