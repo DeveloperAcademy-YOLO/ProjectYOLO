98dbc304d537c57d59636357765a2ce9d47110ff
Kelly-Chui <optioneldar@gmail.com>
2022-10-26 14:29:02 +0900

    [Design] #173 remove unused codes

--- RollingPaper/RollingPaper/Controllers/SidebarViewController.swift
+++ RollingPaper/RollingPaper/Controllers/SidebarViewController.swift
@@ -14,8 +14,8 @@ private class Layout {
     static let userPhotoFrameWidthHeight = 44
     static let userNameFontSize: CGFloat = 20
     static let userPhotoToNamePadding: CGFloat = 16
-    static let userChevronFrameWidth = Int(Double(userInfoStackWidthSuperView) * 0.3 * 0.24)
-    static let userChevronWidthHeight = 15
+    // static let userChevronFrameWidth = Int(Double(userInfoStackWidthSuperView) * 0.3 * 0.24)
+    // static let userChevronWidthHeight = 15
     static let tableCellBackgroundInsets = NSDirectionalEdgeInsets(top: 6, leading: 0, bottom: 6, trailing: 0)
     static let tableCellimageToTextPadding: CGFloat = 16
     static let tableCellInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
@@ -39,30 +39,31 @@ class SidebarViewController: UIViewController, UITableViewDataSource, UITableVie
     private var cancellables = Set<AnyCancellable>()
     private let userPhoto: UIImageView = {
         let photo = UIImageView()
-        photo.image = UIImage(systemName: "person.circle")
         photo.layer.cornerRadius = photo.frame.width / 2
         photo.layer.masksToBounds = true
         photo.contentMode = UIView.ContentMode.scaleAspectFit
         return photo
     }()
     
+    /*
     private let chevron: UIImageView = {
         let chevron = UIImageView()
         chevron.image = UIImage(systemName: "chevron.forward")
         chevron.contentMode = UIView.ContentMode.scaleAspectFit
         return chevron
     }()
+    */
     
     private let userName: UILabel = {
         let name = UILabel()
-        name.text = "요셉"
+        name.text = "Guest"
         name.font = UIFont.preferredFont(forTextStyle: .title3)
         name.sizeToFit()
         return name
     }()
     
     lazy var userInfoStack: UIStackView = {
-        let userInfo = UIStackView(arrangedSubviews: [userPhoto, userName, chevron])
+        let userInfo = UIStackView(arrangedSubviews: [userPhoto, userName])
         userInfo.axis = .horizontal
         userInfo.alignment = .center
         userInfo.distribution = .fillProportionally
@@ -93,12 +94,10 @@ class SidebarViewController: UIViewController, UITableViewDataSource, UITableVie
             .receive(on: DispatchQueue.main)
             .sink { [weak self] userModel in
                 if let userModel = userModel {
-//                    self?.convertURL(from: userModel.profileUrl)
                     self?.userName.text = userModel.name
-                } else {
-//                    self?.userPhoto.image = UIImage(systemName: "person.circle")
+                } /*else {
                     self?.userName.text = "Guest"
-                }
+                }*/
             }
             .store(in: &cancellables)
         viewModel
@@ -107,9 +106,9 @@ class SidebarViewController: UIViewController, UITableViewDataSource, UITableVie
             .sink { [weak self] image in
                 if let image = image {
                     self?.userPhoto.image = image
-                } else {
-                    self?.userPhoto.image = UIImage(systemName: "person.circle")
-                }
+                } /* else {
+                    self?.userPhoto.image = UIImage(systemName: "rectangle")
+                }*/
             }
             .store(in: &cancellables)
     }
@@ -212,7 +211,6 @@ class SidebarViewController: UIViewController, UITableViewDataSource, UITableVie
         userInfoStack.layer.cornerRadius = Layout.userInfoStackRadius
         userInfoStack.isLayoutMarginsRelativeArrangement = true
         userInfoStack.layoutMargins = Layout.userInfoStackInset
-        
         userInfoStack.snp.makeConstraints { make in
             make.leading.equalToSuperview().offset(Layout.userInfoStackLeadingSuperView)
             make.trailing.equalToSuperview().offset(Layout.userInfoStackTrailingSuperView)
@@ -220,11 +218,15 @@ class SidebarViewController: UIViewController, UITableViewDataSource, UITableVie
             make.top.equalTo(view.safeAreaLayoutGuide).offset(Layout.userInfoStackTopSafeArea)
         }
         userPhoto.snp.makeConstraints { make in
-            make.top.height.equalToSuperview().inset(14)
+            make.height.equalTo(userInfoStack.snp.height).offset(-14)
+            make.width.equalTo(userInfoStack.snp.height).offset(-14)
         }
+        userPhoto.layer.cornerRadius = userPhoto.frame.width / 2
+        /*
         chevron.snp.makeConstraints { make in
             make.height.equalToSuperview().multipliedBy(0.24)
         }
+        */
     }
 }
 
