241e24323cabb951b44b516051cf450603b1df39
Yosep <dlstj1212@naver.com>
2022-10-26 23:14:41 +0900

    [feat] #hotfix Add Beige Template for AppStore Push

--- RollingPaper/RollingPaper/Models/TemplateEnum.swift
+++ RollingPaper/RollingPaper/Models/TemplateEnum.swift
@@ -18,7 +18,7 @@ enum TemplateEnum: String, CaseIterable {
     var template: TemplateModel {
         switch self {
         case .white:
-            return TemplateModel(templateString: "basic", templateTitle: "기본", templateDescription: "기본 색깔로 구성된 템플릿", colorHexes: [], stickerNames: [], backgroundImageNames: ["Rectangle_white", "Rectangle_black", "Rectangle_light_pumpkin", "Rectangle_red"])
+            return TemplateModel(templateString: "beige", templateTitle: "베이지", templateDescription: "베이지색으로 구성된 템플릿", colorHexes: [], stickerNames: ["Beige_maple", "Beige_cat", "Beige_dog2", "Beige_dog", "Beige_basket", "Beige_duck", "Beige_gingerbread", "Beige_old_paper", "Beige_ball"], backgroundImageNames: ["Rectangle_beige1", "Rectangle_beige2", "Rectangle_beige3", "Rectangle_beige4"])
         case .halloween:
             return TemplateModel(templateString: "halloween", templateTitle: "할로윈", templateDescription: "달콤하고도 으스스한 스티커와 카드로 이뤄진 테마", colorHexes: [], stickerNames: ["Halloween_Pumpkin", "Halloween_Candy", "Halloween_Bat", "Halloween_Ghost", "Halloween_StickCandy", "Halloween_Pumpkin2", "Halloween_Pot_Green", "Halloween_Pot_Purple", "Halloween_Pot_Red", "Halloween_Hat", "Halloween_Pumpkin_Green", "Halloween_Pumpkin_Orange", "Halloween_Pumpkin_Pale", "Halloween_Ghost2", "Halloween_Ghost_with_legs", "Halloween_Bone", "Halloween_Blood"], backgroundImageNames: ["Rectangle_white", "Pattern_Bone", "Pattern_Pumpkin", "Pattern_Skull"])
 //        case .school:
