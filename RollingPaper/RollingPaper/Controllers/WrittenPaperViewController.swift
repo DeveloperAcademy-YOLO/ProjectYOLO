7b1a7ff564c3ed9adadb7f6664037e955e161afe
dgfghsjd <dgfghsjd@naver.com>
2022-10-27 09:19:45 +0900

    [fix] #175 fix errors after merge dev into here

--- RollingPaper/RollingPaper/Controllers/WrittenPaperViewController.swift
+++ RollingPaper/RollingPaper/Controllers/WrittenPaperViewController.swift
@@ -243,7 +243,8 @@ class WrittenPaperViewController: UIViewController {
     func moveToCardRootView() {
         var isLocalDB: Bool = viewModel.paperFrom == .fromLocal ? true : false
         
-        self.navigationController?.pushViewController(CardRootViewController(viewModel: CardViewModel(), paperID: self.viewModel.currentPaperPublisher.value?.paperId ?? "paperID Send fail", isLocalDB: isLocalDB), animated: true)
+        guard let currentPaper = viewModel.currentPaperPublisher.value else { return }
+        self.navigationController?.pushViewController(CardRootViewController(viewModel: CardViewModel(), paperID: self.viewModel.currentPaperPublisher.value?.paperId ?? "paperID Send fail", isLocalDB: isLocalDB, currentPaper: currentPaper), animated: true)
     }
     
     func presentSignUpModal(_ sender: UIButton) {
