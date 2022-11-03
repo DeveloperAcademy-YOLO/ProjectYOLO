2e4d96d55246682db947e76a49062c1aa329b5e8
ddophi98 <lightcloud98@gmail.com>
2022-11-03 20:53:58 +0900

    [refactor] #201 refactor code

--- RollingPaper/RollingPaper/Controllers/MagnifiedCardViewController.swift
+++ RollingPaper/RollingPaper/Controllers/MagnifiedCardViewController.swift
@@ -19,6 +19,16 @@ class MagnifiedCardViewController: UIViewController, FSPagerViewDataSource, FSPa
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setView()
+        setImagesOrder()
+        setPagerView()
+    }
+    // 현재 창 닫기
+    @objc func closeAction() {
+        dismiss(animated: true)
+    }
+    // 뷰 설정하기
+    func setView() {
         view.isOpaque = false
         view.backgroundColor = .clear
         view.addSubview(closeBtn)
@@ -27,22 +37,15 @@ class MagnifiedCardViewController: UIViewController, FSPagerViewDataSource, FSPa
 
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(closeAction))
         view.addGestureRecognizer(tapGesture)
-        setImagesOrder()
-        setPagerView()
     }
-    
-    @objc func closeAction() {
-        dismiss(animated: true)
-    }
-    
+    // 현재 선택한 이미지에 맞춰서 이미지 순서 바꾸기
     func setImagesOrder() {
         let front = Array(images[selectedIndex..<images.count])
         let back = Array(images[0..<selectedIndex])
         images = front + back
     }
-    
+    // 페이저 설정하기
     func setPagerView() {
-        // Create a pager view
         pagerView.snp.makeConstraints({ make in
             make.width.equalTo(self.view.bounds.width * 0.75)
             make.height.equalTo(self.view.bounds.width * 0.75 * 0.75)
@@ -68,11 +71,11 @@ class MagnifiedCardViewController: UIViewController, FSPagerViewDataSource, FSPa
         pageControl.setFillColor(.systemGray, for: .selected)
         pageControl.setFillColor(.systemGray3, for: .normal)
     }
-    
+    // 페이지 개수
     func numberOfItems(in pagerView: FSPagerView) -> Int {
         return images.count
     }
-    
+    // 페이지마다 보여지는 셀
     func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSPagerViewCell {
         let cell = pagerView.dequeueReusableCell(withReuseIdentifier: "cell", at: index)
         cell.imageView?.image = images[index]
@@ -81,15 +84,14 @@ class MagnifiedCardViewController: UIViewController, FSPagerViewDataSource, FSPa
         cell.imageView?.clipsToBounds = true
         return cell
     }
-    
+    // 페이지 이동에 따라 컨트롤러도 바뀌기 (아래 점점점 나오는거)
     func pagerViewWillEndDragging(_ pagerView: FSPagerView, targetIndex: Int) {
         pageControl.currentPage = (selectedIndex+targetIndex)%images.count
     }
-    
     func pagerViewDidEndScrollAnimation(_ pagerView: FSPagerView) {
         pageControl.currentPage = (selectedIndex+pagerView.currentIndex)%images.count
     }
-    
+    // 사진 클릭 안되도록 하기
     func pagerView(_ pagerView: FSPagerView, shouldHighlightItemAt index: Int) -> Bool {
         return false
     }
