0158474e2aa71a2b76665271fc4bcabe0950d044
Kelly-Chui <optioneldar@gmail.com>
2022-11-11 10:18:21 +0900

    [feat] #230 change icon color when selected

--- RollingPaper/RollingPaper/ViewControllers/Core/Sidebar/SidebarViewController.swift
+++ RollingPaper/RollingPaper/ViewControllers/Core/Sidebar/SidebarViewController.swift
@@ -222,7 +222,7 @@ class CategoryCell: UICollectionViewListCell {
             state.isSelected || state.isHighlighted ? .black : .black
         }
         contentConfig.image = UIImage(systemName: state.isSelected || state.isHighlighted ? categoryData[1] + ".fill" : categoryData[1])
-        
+        contentConfig.imageProperties.tintColor = state.isSelected || state.isHighlighted ? .black : .systemGray3
         guard var backgroundConfig = self.backgroundConfiguration?.updated(for: state) else { return }
         backgroundConfig.backgroundColorTransformer = UIConfigurationColorTransformer { _ in
             state.isSelected || state.isHighlighted ? .white : .clear
