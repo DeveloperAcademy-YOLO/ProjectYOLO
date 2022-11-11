6f960721a84d1eff982b4b81661043277400bc3b
Grizzly <dydrhks02@naver.com>
2022-11-11 13:46:42 +0900

    [feat] #259 check deletePaper duplication

--- RollingPaper/RollingPaper/ViewControllers/Other/WrittenPaper/WrittenPaperViewController.swift
+++ RollingPaper/RollingPaper/ViewControllers/Other/WrittenPaper/WrittenPaperViewController.swift
@@ -321,23 +321,23 @@ final class WrittenPaperViewController: UIViewController {
         present(allertController, animated: true)
     }
     
-    func deletePaper() {
-        let deleteVerifyText = self.titleEmbedingTextField.text
-        if deleteVerifyText == self.viewModel.currentPaper?.title {
-            if viewModel.isPaperLinkMade { //링크가 만들어진 것이 맞다면 서버에 페이퍼가 저장되어있으므로
-                viewModel.deletePaper(viewModel.currentPaper!.paperId, from: .fromServer)
-            } else {
-                viewModel.deletePaper(viewModel.currentPaper!.paperId, from: .fromLocal)
-            }
-            self.moveToPaperStorageView()
-        } else {
-            let alert = UIAlertController(title: "제목을 잘못 입력하셨습니다", message: nil, preferredStyle: .alert)
-            let confirm = UIAlertAction(title: "확인", style: .default)
-            alert.addAction(confirm)
-            alert.preferredAction = confirm
-            self.present(alert, animated: true, completion: nil)
-        }
-    }
+//    func deletePaper() {
+//        let deleteVerifyText = self.titleEmbedingTextField.text
+//        if deleteVerifyText == self.viewModel.currentPaper?.title {
+//            if viewModel.isPaperLinkMade { //링크가 만들어진 것이 맞다면 서버에 페이퍼가 저장되어있으므로
+//                viewModel.deletePaper(viewModel.currentPaper!.paperId, from: .fromServer)
+//            } else {
+//                viewModel.deletePaper(viewModel.currentPaper!.paperId, from: .fromLocal)
+//            }
+//            self.moveToPaperStorageView()
+//        } else {
+//            let alert = UIAlertController(title: "제목을 잘못 입력하셨습니다", message: nil, preferredStyle: .alert)
+//            let confirm = UIAlertAction(title: "확인", style: .default)
+//            alert.addAction(confirm)
+//            alert.preferredAction = confirm
+//            self.present(alert, animated: true, completion: nil)
+//        }
+//    }
     
     func setCollectionView() -> UICollectionView {
         var cardsCollection: UICollectionView?
