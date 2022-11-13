24d493ea2b5044c770f037d59c1e27a8767ba79b
dgfghsjd <dgfghsjd@naver.com>
2022-11-13 18:52:52 +0900

    [feat] #265  dismiss card by swiping

--- RollingPaper/RollingPaper/ViewControllers/Other/WrittenPaper/MagnifiedCardViewController.swift
+++ RollingPaper/RollingPaper/ViewControllers/Other/WrittenPaper/MagnifiedCardViewController.swift
@@ -46,7 +46,28 @@ class MagnifiedCardViewController: UIViewController {
         view.addSubview(closeBtn)
         view.addSubview(cardsCollectionView)
         setCloseBtn()
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handleDismiss)))
     }
+    var viewTranslation = CGPoint(x: 0, y: 0)
+        @objc func handleDismiss(sender: UIPanGestureRecognizer) {
+            switch sender.state {
+            case .changed:
+                viewTranslation = sender.translation(in: view)
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                    self.view.transform = CGAffineTransform(translationX: 0, y: self.viewTranslation.y)
+                })
+            case .ended:
+                    if viewTranslation.y < 200 {
+                        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                            self.view.transform = .identity
+                        })
+                    } else {
+                        dismiss(animated: true, completion: nil)
+                    }
+            default:
+                break
+            }
+        }
     
     @objc func closeAction() {
         dismiss(animated: true)
