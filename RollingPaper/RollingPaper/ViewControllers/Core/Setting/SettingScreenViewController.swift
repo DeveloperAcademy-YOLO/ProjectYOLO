55698d7ba6091ca254809cc2058d811d683a8eba
Grizzly <dydrhks02@naver.com>
2022-11-14 15:32:18 +0900

    [feat] #274 code line mediate

--- RollingPaper/RollingPaper/ViewControllers/Core/Setting/SettingScreenViewController.swift
+++ RollingPaper/RollingPaper/ViewControllers/Core/Setting/SettingScreenViewController.swift
@@ -14,7 +14,7 @@ import SnapKit
 import UIKit
 
 class SettingScreenViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
-
+    
     private let viewModel: SettingScreenViewModel = SettingScreenViewModel()
     private var cancellables = Set<AnyCancellable>()
     private let input: PassthroughSubject<SettingScreenViewModel.Input, Never> = .init()
@@ -31,7 +31,7 @@ class SettingScreenViewController: UIViewController, UIImagePickerControllerDele
         button.setTitleColor(.systemBlue, for: .normal)
         return button
     }()
-
+    
     private let cancelButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = UIColor.systemBackground
@@ -41,7 +41,7 @@ class SettingScreenViewController: UIViewController, UIImagePickerControllerDele
         button.setTitleColor(.systemBlue, for: .normal)
         return button
     }()
-
+    
     private let profileImage: UIImageView = {
         let profileImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 180, height: 180))
         profileImage.layer.cornerRadius = profileImage.frame.size.width * 0.5
@@ -52,7 +52,7 @@ class SettingScreenViewController: UIViewController, UIImagePickerControllerDele
         profileImage.clipsToBounds = true
         return profileImage
     }()
-
+    
     private let editPhotoButton: UIButton = {
         let button = UIButton()
         button.setBackgroundImage(UIImage(systemName: "photo.on.rectangle.angled"), for: .normal)
@@ -67,7 +67,7 @@ class SettingScreenViewController: UIViewController, UIImagePickerControllerDele
         textField.isHidden = true
         return textField
     }()
-
+    
     private let profileLabel: UILabel = {
         let textLabel = UILabel()
         textLabel.text = "Guest"
@@ -75,7 +75,7 @@ class SettingScreenViewController: UIViewController, UIImagePickerControllerDele
         textLabel.textAlignment = .center
         return textLabel
     }()
-
+    
     private let divideView: UIView = {
         let view = UIView()
         view.backgroundColor = UIColor.gray
@@ -83,7 +83,7 @@ class SettingScreenViewController: UIViewController, UIImagePickerControllerDele
         view.layer.borderColor = UIColor.systemPink.cgColor
         return view
     }()
-
+    
     private let logoutButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = UIColor.systemBackground
@@ -93,7 +93,7 @@ class SettingScreenViewController: UIViewController, UIImagePickerControllerDele
         button.setAttributedTitle(title, for: .normal)
         return button
     }()
-
+    
     private let resignButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = UIColor.systemBackground
@@ -103,7 +103,7 @@ class SettingScreenViewController: UIViewController, UIImagePickerControllerDele
         button.setAttributedTitle(title, for: .normal)
         return button
     }()
-
+    
     private let visualEffectView: UIVisualEffectView = {
         let blurEffect = UIBlurEffect(style: .dark)
         let visualEffectView = UIVisualEffectView(effect: blurEffect)
@@ -161,11 +161,11 @@ class SettingScreenViewController: UIViewController, UIImagePickerControllerDele
                 }
             }
             .store(in: &cancellables)
-
+        
         editPhotoButton
             .tapPublisher
-            .sink { _ in
-                self.presentImagePicker(withType: .photoLibrary)
+            .sink { [weak self] in
+                self?.presentImagePicker(withType: .photoLibrary)
             }
             .store(in: &cancellables)
         
@@ -218,7 +218,7 @@ class SettingScreenViewController: UIViewController, UIImagePickerControllerDele
             }
         }
     }
-
+    
     private func presentImagePicker(withType type: UIImagePickerController.SourceType) {
         let pickerController = UIImagePickerController()
         pickerController.delegate = self
@@ -242,36 +242,36 @@ class SettingScreenViewController: UIViewController, UIImagePickerControllerDele
     }
     
     @objc private func logOutBtnPressed(_ gesture: UITapGestureRecognizer) {
-            let alert = UIAlertController(title: "로그아웃", message: "로그아웃 하시겠습니까?", preferredStyle: .alert)
+        let alert = UIAlertController(title: "로그아웃", message: "로그아웃 하시겠습니까?", preferredStyle: .alert)
         
-            alert.addAction(UIAlertAction(title: "취소", style: .default, handler: { (_: UIAlertAction!) in
-                alert.dismiss(animated: true, completion: nil)
-               }))
-            
-            alert.addAction(UIAlertAction(title: "로그아웃", style: .destructive, handler: { (_: UIAlertAction!) in
-                self.input.send(.signOutDidTap)
-               }))
-            present(alert, animated: true)
-        }
+        alert.addAction(UIAlertAction(title: "취소", style: .default, handler: { (_: UIAlertAction!) in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "로그아웃", style: .destructive, handler: { (_: UIAlertAction!) in
+            self.input.send(.signOutDidTap)
+        }))
+        present(alert, animated: true)
+    }
     
     @objc private func resignBtnPressed(_ gesture: UITapGestureRecognizer) {
-            let alert = UIAlertController(title: " 회원 탈퇴를 하면 모든 기록이 사라집니다.", message: "회원 탈퇴를 하시겠습니까?", preferredStyle: .alert)
+        let alert = UIAlertController(title: " 회원 탈퇴를 하면 모든 기록이 사라집니다.", message: "회원 탈퇴를 하시겠습니까?", preferredStyle: .alert)
         
-            alert.addAction(UIAlertAction(title: "취소", style: .default, handler: { (_: UIAlertAction!) in
-                alert.dismiss(animated: true, completion: nil)
-               }))
-            
-            alert.addAction(UIAlertAction(title: "회원 탈퇴", style: .destructive, handler: { (_: UIAlertAction!) in
-                self.input.send(.resignDidTap)
-               }))
-            present(alert, animated: true)
-        }
-
+        alert.addAction(UIAlertAction(title: "취소", style: .default, handler: { (_: UIAlertAction!) in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "회원 탈퇴", style: .destructive, handler: { (_: UIAlertAction!) in
+            self.input.send(.resignDidTap)
+        }))
+        present(alert, animated: true)
+    }
+    
     @objc private func didCancelButton() {
         profileText.textField.resignFirstResponder()
         navigationItem.rightBarButtonItem?.title = "편집"
         navigationItem.leftBarButtonItem?.title = nil
-
+        
         divideView.isHidden = false
         logoutButton.isHidden = false
         resignButton.isHidden = false
@@ -286,7 +286,7 @@ class SettingScreenViewController: UIViewController, UIImagePickerControllerDele
         profileText.textField.attributedPlaceholder = NSAttributedString(string: self.profileText.textField.text ?? "", attributes: [NSAttributedString.Key.foregroundColor: UIColor.systemGray, NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .body)])
         profileImage.image = viewModel.currentPhotoSubject.value ?? UIImage(systemName: "person")
     }
-
+    
     @objc private func didEditButton() {
         if let currentTitle = navigationItem.rightBarButtonItem?.title {
             if currentTitle == "편집" {
@@ -305,11 +305,6 @@ class SettingScreenViewController: UIViewController, UIImagePickerControllerDele
                 profileText.textField.sendActions(for: .editingChanged)
                 profileText.textField.sendActions(for: .editingDidEnd)
                 if profileText.passedSubject.value {
-                    // 이름만 변경
-                    // 사진만 변경
-                    // 이름, 사진 모두 변경 완료
-                    // 이름, 사진 모두 같을 때 -> 아무 것도 인풋으로 send하지 않음
-                    
                     if self.profileImage.image == viewModel.currentPhotoSubject.value {
                         // 사진 변경 X
                         if self.viewModel.currentUserSubject.value?.name == self.profileText.textField.text {
@@ -334,12 +329,12 @@ class SettingScreenViewController: UIViewController, UIImagePickerControllerDele
             }
         }
     }
-
+    
     @objc private func didBackgroundTap() {
         view.endEditing(true)
         profileText.textField.resignFirstResponder()
     }
-
+    
     @objc private func keyboardWillShow(notification: NSNotification) {
         if
             let userInfo = notification.userInfo,
@@ -351,7 +346,7 @@ class SettingScreenViewController: UIViewController, UIImagePickerControllerDele
             }
         }
     }
-
+    
     @objc private func keyboardWillHide(notification: NSNotification) {
         if view.frame.origin.y != 0 {
             view.frame.origin.y = 0
@@ -363,66 +358,66 @@ class SettingScreenViewController: UIViewController, UIImagePickerControllerDele
 extension SettingScreenViewController {
     private func setupLayout() {
         view.backgroundColor = .systemBackground
-
+        
         profileImage.addSubview(visualEffectView)
         profileImage.addSubview(editPhotoButton)
-
-        view.addSubviews([profileImage,profileText,profileLabel,divideView,logoutButton,resignButton,editButton])
+        
+        view.addSubviews([profileImage, profileText, profileLabel, divideView, logoutButton, resignButton, editButton])
         
         let backgroundTap = UITapGestureRecognizer(target: self, action: #selector(didBackgroundTap))
         view.addGestureRecognizer(backgroundTap)
-
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "편집", style: .done, target: self, action: #selector(didEditButton))
-
+        
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "", style: .done, target: self, action: #selector(didCancelButton))
-
+        
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-
+        
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-
+        
         profileImage.snp.makeConstraints ({ make in
             make.top.equalTo(200)
             make.centerX.equalToSuperview()
             make.height.equalTo(180)
             make.width.equalTo(180)
         })
-
+        
         editPhotoButton.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview()
             make.height.equalTo(39)
             make.width.equalTo(48)
         }
-
+        
         profileLabel.snp.makeConstraints { make in
             make.top.equalTo(profileImage.snp.bottom).offset(47)
             make.width.equalTo(318)
             make.centerX.equalToSuperview()
         }
-
+        
         profileText.snp.makeConstraints { make in
             make.top.equalTo(profileImage.snp.bottom).offset(47)
             make.width.equalTo(318)
             make.centerX.equalToSuperview()
         }
-
+        
         profileText.textField.text = viewModel.currentUserSubject.value?.name
         profileText.textField.sendActions(for: .editingChanged)
         profileText.setTextFieldType(type: .name)
         profileText.setWaringView(waringShown: false, text: nil)
-
+        
         divideView.snp.makeConstraints ({ make in
             make.top.equalTo(profileLabel.snp.bottom).offset(26)
             make.centerX.equalToSuperview()
             make.height.equalTo(2)
             make.width.equalTo(264)
         })
-
+        
         logoutButton.snp.makeConstraints({ make in
             make.top.equalTo(divideView.snp.bottom).offset(25)
             make.centerX.equalToSuperview()
         })
-
+        
         resignButton.snp.makeConstraints({ make in
             make.top.equalTo(logoutButton.snp.bottom).offset(16.5)
             make.centerX.equalToSuperview()
