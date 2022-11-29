deca98fe82eecd44e0710b0cd6a45ab84862900c
Yosep <dlstj1212@naver.com>
2022-11-29 15:41:22 +0900

    [design] #359 Add Nature template

--- RollingPaper/RollingPaper/Models/TemplateEnum.swift
+++ RollingPaper/RollingPaper/Models/TemplateEnum.swift
@@ -13,6 +13,7 @@ enum TemplateEnum: String, CaseIterable {
     case wedding
     case christmas
     case beach
+    case nature
     
     var template: TemplateModel {
         switch self {
@@ -26,8 +27,8 @@ enum TemplateEnum: String, CaseIterable {
             return TemplateModel(templateString: "christmas", templateTitle: "크리스마스", templateDescription: "흰눈이 내리는 크리스마스", colorHexes: [], stickerNames: ["Christmas_Bell", "Christmas_Cookie", "Christmas_Decoration", "Christmas_Decoration2", "Christmas_Hat", "Christmas_Penguin1", "Christmas_Penguin2", "Christmas_Reindeer", "Christmas_Reindeer2", "Christmas_Santa", "Christmas_Santa2", "Christmas_Santa3", "Christmas_Santa4"], backgroundImageNames: ["Pattern_Decoration", "Pattern_MoonChristmas", "Pattern_RedChristmas", "Pattern_WhiteChristmas"])
         case .beach:
             return TemplateModel(templateString: "beach", templateTitle: "해변", templateDescription: "시원한 해변가로 떠나요", colorHexes: [], stickerNames: ["Beach_ball", "Beach_bucket", "Beach_lemonade", "Beach_palmtree", "Beach_parasole", "Beach_seat", "Beach_shell1", "Beach_shell2", "Beach_yacht"], backgroundImageNames: ["Pattern_beach1", "Pattern_beach2", "Pattern_beach3", "Pattern_beach4"])
-//        case .sunrise:
-//            return TemplateModel(templateString: "sunrise", templateTitle: "일출", templateDescription: "일출일출", colorHexes: [], stickerNames: [], backgroundImageNames: [])
+        case .nature:
+            return TemplateModel(templateString: "nature", templateTitle: "자연", templateDescription: "초록초록 자연의 상쾌함", colorHexes: [], stickerNames: ["Nature_Sticker1", "Nature_Sticker2", "Nature_Sticker3", "Nature_Sticker4", "Nature_Sticker5", "Nature_Sticker6", "Nature_Sticker7", "Nature_Sticker8", "Nature_Sticker9"], backgroundImageNames: ["Pattern_nature1", "Pattern_nature2", "Pattern_nature3", "Pattern_nature4"])
         }
     }
 }
