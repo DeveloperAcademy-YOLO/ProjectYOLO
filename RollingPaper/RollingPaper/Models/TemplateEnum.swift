60b41c0052deedc240ce03f794eb1aa7f631770f
Yosep <dlstj1212@naver.com>
2022-11-30 10:10:15 +0900

    [design] #359 Add Retro & Bear Template

--- RollingPaper/RollingPaper/Models/TemplateEnum.swift
+++ RollingPaper/RollingPaper/Models/TemplateEnum.swift
@@ -14,6 +14,8 @@ enum TemplateEnum: String, CaseIterable {
     case christmas
     case beach
     case nature
+    case retro
+    case bear
     
     var template: TemplateModel {
         switch self {
@@ -24,11 +26,15 @@ enum TemplateEnum: String, CaseIterable {
         case .wedding:
             return TemplateModel(templateString: "wedding", templateTitle: "웨딩", templateDescription: "소중한 사람의 결혼을 축하해요.", colorHexes: [], stickerNames: ["wedding_bridegroom", "wedding_brird", "wedding_couple", "wedding_coupleCutepng", "wedding_flowers", "wedding_heartSticker", "wedding_Ringflower1", "wedding_Ringflower2", "wedding_Ringheart", "wedding_wedding car"], backgroundImageNames: ["Rectangle_white", "Pattern_wedding1", "Pattern_wedding2", "Pattern_wedding3"])
         case .christmas:
-            return TemplateModel(templateString: "christmas", templateTitle: "크리스마스", templateDescription: "흰눈이 내리는 크리스마스", colorHexes: [], stickerNames: ["Christmas_Bell", "Christmas_Cookie", "Christmas_Decoration", "Christmas_Decoration2", "Christmas_Hat", "Christmas_Penguin1", "Christmas_Penguin2", "Christmas_Reindeer", "Christmas_Reindeer2", "Christmas_Santa", "Christmas_Santa2", "Christmas_Santa3", "Christmas_Santa4"], backgroundImageNames: ["Pattern_Decoration", "Pattern_MoonChristmas", "Pattern_RedChristmas", "Pattern_WhiteChristmas"])
+            return TemplateModel(templateString: "christmas", templateTitle: "크리스마스", templateDescription: "흰눈이 내리는 크리스마스", colorHexes: [], stickerNames: ["Christmas_Bell", "Christmas_Cookie", "Christmas_Decoration", "Christmas_Decoration2", "Christmas_Hat", "Christmas_Penguin1", "Christmas_Penguin2", "Christmas_Reindeer", "Christmas_Reindeer2", "Christmas_Santa", "Christmas_Santa2", "Christmas_Santa3", "Christmas_Santa4", "Christmas_Snowman"], backgroundImageNames: ["Pattern_Decoration", "Pattern_MoonChristmas", "Pattern_RedChristmas", "Pattern_WhiteChristmas"])
         case .beach:
             return TemplateModel(templateString: "beach", templateTitle: "해변", templateDescription: "시원한 해변가로 떠나요", colorHexes: [], stickerNames: ["Beach_ball", "Beach_bucket", "Beach_lemonade", "Beach_palmtree", "Beach_parasole", "Beach_seat", "Beach_shell1", "Beach_shell2", "Beach_yacht"], backgroundImageNames: ["Pattern_beach1", "Pattern_beach2", "Pattern_beach3", "Pattern_beach4"])
         case .nature:
             return TemplateModel(templateString: "nature", templateTitle: "자연", templateDescription: "초록초록 자연의 상쾌함", colorHexes: [], stickerNames: ["Nature_Sticker1", "Nature_Sticker2", "Nature_Sticker3", "Nature_Sticker4", "Nature_Sticker5", "Nature_Sticker6", "Nature_Sticker7", "Nature_Sticker8", "Nature_Sticker9"], backgroundImageNames: ["Pattern_nature1", "Pattern_nature2", "Pattern_nature3", "Pattern_nature4"])
+        case .retro:
+            return TemplateModel(templateString: "retro", templateTitle: "레트로", templateDescription: "레트로 감성을 느껴보세요.", colorHexes: [], stickerNames: ["Retro_Retro1", "Retro_Retro2", "Retro_Retro3", "Retro_Retro4", "Retro_Retro5", "Retro_Retro6", "Retro_Retro7", "Retro_Retro8", "Retro_Retro9", "Retro_Retro10", "Retro_Retro11"], backgroundImageNames: ["Pattern_Retro1", "Pattern_Retro2", "Pattern_Retro3", "Pattern_Retro4"])
+        case .bear:
+            return TemplateModel(templateString: "bear", templateTitle: "곰돌이", templateDescription: "앙증맞는 곰돌이 스티커가 들어있어요.", colorHexes: [], stickerNames: ["Bear_Sticker12", "Bear_Sticker11", "Bear_Sticker10", "Bear_Sticker4", "Bear_Sticker5", "Bear_Sticker6", "Bear_Sticker7", "Bear_Sticker8", "Bear_Sticker9", "Bear_Sticker3", "Bear_Sticker2", "Bear_Sticker1"], backgroundImageNames: ["Rectangle_white", "Pattern_bear1", "Pattern_Retro3", "Pattern_Retro4"])
         }
     }
 }
