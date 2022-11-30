66d5e39f4c0a150b0c91ba6814d49f8f139c7799
Yosep <dlstj1212@naver.com>
2022-11-30 11:36:40 +0900

    [design] #359 Add birthDay Template

--- RollingPaper/RollingPaper/Models/TemplateEnum.swift
+++ RollingPaper/RollingPaper/Models/TemplateEnum.swift
@@ -10,8 +10,9 @@ import Foundation
 enum TemplateEnum: String, CaseIterable {
     case beige
     case halloween
-    case wedding
+    case birthDay
     case christmas
+    case wedding
     case beach
     case nature
     case retro
@@ -20,13 +21,15 @@ enum TemplateEnum: String, CaseIterable {
     var template: TemplateModel {
         switch self {
         case .beige:
-            return TemplateModel(templateString: "beige", templateTitle: "베이지", templateDescription: "베이지색으로 구성된 테마", colorHexes: [], stickerNames: ["Beige_maple", "Beige_cat", "Beige_dog2", "Beige_dog", "Beige_basket", "Beige_duck", "Beige_gingerbread", "Beige_old_paper", "Beige_ball"], backgroundImageNames: ["Pattern_beige1", "Pattern_beige2", "Pattern_beige3", "Pattern_beige4"])
+            return TemplateModel(templateString: "beige", templateTitle: "베이지", templateDescription: "베이지색으로 구성된 테마", colorHexes: [], stickerNames: ["Beige_redFox", "Beige_moon", "Beige_lioness", "Beige_lion", "Beige_dog3", "Beige_dog1", "Beige_coffee", "Beige_catCup", "Beige_autumnTree", "Beige_autumnLeaf", "Beige_maple", "Beige_cat", "Beige_dog2", "Beige_dog", "Beige_basket", "Beige_duck", "Beige_gingerbread", "Beige_old_paper", "Beige_ball"], backgroundImageNames: ["Pattern_beige1", "Pattern_beige2", "Pattern_beige3", "Pattern_beige4"])
         case .halloween:
-            return TemplateModel(templateString: "halloween", templateTitle: "할로윈", templateDescription: "달콤하고도 으스스한 스티커와 카드로 이뤄진 테마", colorHexes: [], stickerNames: ["Halloween_Pumpkin", "Halloween_Candy", "Halloween_Bat", "Halloween_Ghost", "Halloween_StickCandy", "Halloween_Pumpkin2", "Halloween_Pot_Green", "Halloween_Pot_Purple", "Halloween_Pot_Red", "Halloween_Hat", "Halloween_choco", "Halloween_Pumpkin_Green", "Halloween_Pumpkin_Orange", "Halloween_Pumpkin_Pale", "Halloween_Ghost2", "Halloween_Ghost_with_legs", "Halloween_Bone", "Halloween_house", "Halloween_hat2", "Halloween_hand", "Halloween_cat", "Halloween_bat2"], backgroundImageNames: ["Rectangle_white", "Pattern_Bone", "Pattern_Pumpkin", "Pattern_Skull"])
-        case .wedding:
-            return TemplateModel(templateString: "wedding", templateTitle: "웨딩", templateDescription: "소중한 사람의 결혼을 축하해요.", colorHexes: [], stickerNames: ["wedding_bridegroom", "wedding_brird", "wedding_couple", "wedding_coupleCutepng", "wedding_flowers", "wedding_heartSticker", "wedding_Ringflower1", "wedding_Ringflower2", "wedding_Ringheart", "wedding_wedding car"], backgroundImageNames: ["Rectangle_white", "Pattern_wedding1", "Pattern_wedding2", "Pattern_wedding3"])
+            return TemplateModel(templateString: "halloween", templateTitle: "할로윈", templateDescription: "달콤하고도 으스스한 스티커와 카드로 이뤄진 테마", colorHexes: [], stickerNames: ["Halloween_Pumpkin", "Halloween_Candy", "Halloween_Bat", "Halloween_Ghost", "Halloween_StickCandy", "Halloween_Pumpkin2", "Halloween_Pot_Green", "Halloween_Pot_Purple", "Halloween_Pot_Red", "Halloween_Hat", "Halloween_cat2", "Halloween_choco", "Halloween_Pumpkin_Green", "Halloween_Pumpkin_Orange", "Halloween_Pumpkin_Pale", "Halloween_Ghost2", "Halloween_happy", "Halloween_footprint", "Halloween_Ghost_with_legs", "Halloween_Bone", "Halloween_house", "Halloween_hat2", "Halloween_hand", "Halloween_cat", "Halloween_bat2"], backgroundImageNames: ["Rectangle_white", "Pattern_Bone", "Pattern_Pumpkin", "Pattern_Skull"])
+        case .birthDay:
+            return TemplateModel(templateString: "birthDay", templateTitle: "생일파티", templateDescription: "신나는 생일파티 컨셉의 테마", colorHexes: [], stickerNames: ["BirthDay", "BirthDay_Bear_Black", "BirthDay_Bear_Blue", "BirthDay_Bear_Green", "BirthDay_Bear_Red", "BirthDay_Bear_Yellow", "BirthDay_Cake_Low_Black", "BirthDay_Cake_Low_Green", "BirthDay_HBDCake_Yellow", "Cake_Low_White", "Cake_Low_Yellow", "Cupcake_Black", "Cupcake_Blue", "Cupcake_CreamBlue", "Cupcake_CreamPink", "Cupcake_Green", "Cupcake_Pink", "Cupcake_White", "Cupcake_Yellow", "Firecracker_Blue", "Firecracker_Pink", "Firecracker_Yellow", "HBDCake_Black", "HBDCake_Blue", "HBDCake_Red"], backgroundImageNames: ["Cake_Blue", "Cake_Note_Black", "Cake_Note_White", "Cake_Yellow"])
         case .christmas:
             return TemplateModel(templateString: "christmas", templateTitle: "크리스마스", templateDescription: "흰눈이 내리는 크리스마스", colorHexes: [], stickerNames: ["Christmas_Bell", "Christmas_Cookie", "Christmas_Decoration", "Christmas_Decoration2", "Christmas_Hat", "Christmas_Penguin1", "Christmas_Penguin2", "Christmas_Reindeer", "Christmas_Reindeer2", "Christmas_Santa", "Christmas_Santa2", "Christmas_Santa3", "Christmas_Santa4", "Christmas_Snowman"], backgroundImageNames: ["Pattern_Decoration", "Pattern_MoonChristmas", "Pattern_RedChristmas", "Pattern_WhiteChristmas"])
+        case .wedding:
+            return TemplateModel(templateString: "wedding", templateTitle: "웨딩", templateDescription: "소중한 사람의 결혼을 축하해요.", colorHexes: [], stickerNames: ["wedding_bridegroom", "wedding_brird", "wedding_couple", "wedding_coupleCutepng", "wedding_flowers", "wedding_heartSticker", "wedding_Ringflower1", "wedding_Ringflower2", "wedding_Ringheart", "wedding_wedding car"], backgroundImageNames: ["Rectangle_white", "Pattern_wedding1", "Pattern_wedding2", "Pattern_wedding3"])
         case .beach:
             return TemplateModel(templateString: "beach", templateTitle: "해변", templateDescription: "시원한 해변가로 떠나요", colorHexes: [], stickerNames: ["Beach_ball", "Beach_bucket", "Beach_lemonade", "Beach_palmtree", "Beach_parasole", "Beach_seat", "Beach_shell1", "Beach_shell2", "Beach_yacht"], backgroundImageNames: ["Pattern_beach1", "Pattern_beach2", "Pattern_beach3", "Pattern_beach4"])
         case .nature:
